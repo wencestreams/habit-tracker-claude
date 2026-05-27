07cb56d14109e829381e329f48f35a04bf760ca1
vKasan <vkasan@wence.cz>
2026-05-27 14:34:46 +0200

    v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'habit-tracker-v4';
+const CACHE = 'habit-tracker-v5';
 const LOCAL_ASSETS = ['./', 'index.html', 'manifest.json', 'icon.svg'];
 
 self.addEventListener('install', e => {
